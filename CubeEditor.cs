1dc9f3212db4453fcfbc3610b4fd352c57e014f2
Taya22 <64900670+Taya22@users.noreply.github.com>
2020-09-28 14:21:34 +0300

    Cube Editor (ExecuteInEditMode)

---
+++ CubeEditor.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+[SelectionBase]
+[RequireComponent(typeof(Waypoint))]
+public class CubeEditor : MonoBehaviour
+{
+
+    private Waypoint waypoint;
+
+    private void Awake()
+    {
+        waypoint = GetComponent<Waypoint>();
+    }
+    
+    void Update()
+    {
+        //SnapToGrid();
+        UpdateLable();
+    }
+
+    // private void SnapToGrid()
+    // {
+    //     int gridSize = waypoint.GetGridRange();
+    //     transform.position = new Vector3(
+    //         waypoint.GetGridPos().x * gridSize, 
+    //         0f, 
+    //         waypoint.GetGridPos().y * gridSize
+    //         );
+    // }
+
+    private void UpdateLable()
+    {
+        string lableText = waypoint.GetGridPos().x + "," + waypoint.GetGridPos().y;
+        TextMesh textMesh = GetComponentInChildren<TextMesh>();
+        textMesh.text = lableText;
+        gameObject.name = lableText;
+    }
+}
